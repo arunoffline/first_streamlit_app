79bd2b51f07bf4d4a6a579453f38ff587a8d6770
arunoffline <93048630+arunoffline@users.noreply.github.com>
2023-06-21 19:53:39 -0700

    Update streamlit_app.py
    commit updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Dinner')
+streamlit.title('My Moms New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text("🥣 Omega 3 & Blueberry Oat Meal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
   
