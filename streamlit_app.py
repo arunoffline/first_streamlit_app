f655669940815c6443aa2da9ae5c46fb39add7d0
arunoffline <93048630+arunoffline@users.noreply.github.com>
2023-06-21 19:46:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('This is a test application')
+
+streamlit.header('Header value')
+streamlit.text("Text Value1")
+streamlit.text("Text Value2")
+streamlit.text("Text Value3")
